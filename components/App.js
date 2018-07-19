a044b5774c7f6f6a4170c294f0f3d38fadcf55d1
Maja Jelenik <maja.jelenik@vp.pl>
2018-07-19 16:47:23 +0200

    Change js files

--- components/App.js
+++ components/App.js
@@ -26,12 +26,12 @@ var contactForm = {
 };
 
 var App = React.createClass({
-  render: function() {
-    return (
-      <div className={'app'}>{
-        React.createElement(Contact Form, {contact: contactForm}),
-        React.createElement(Contacts, {items: contacts}, {})}
-      </div>
-    );
-  }
+    render: function() {
+        return (
+            <div className={'app'}>
+                <ContactForm contact={contactForm} />
+                <Contacts items={contacts} />
+            </div>
+        );
+    }
 });
